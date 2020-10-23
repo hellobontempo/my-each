5d043f20e5b60228e757f6e385cad27fb6d8c50c
claire <clairebontempo@gmail.com>
2020-10-23 10:45:41 -0700

    commit tree 72214fed59007993b9c36b86a4ea36d297a5b54b

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+
+def my_each(array) 
+  i = 0
+  while i < array.length
+    yield array[i]
+    i = i + 1
+  end
+array
+      # code here
+  # use while to loop each element of array
+  # yield each element contained in array to a block
 end
